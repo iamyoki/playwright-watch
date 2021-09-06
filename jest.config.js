eb767e54492315ca37d98c4ddf6a1c7679ad557b
Yoki Yu <yokiyuqian@gmail.com>
2021-09-06 16:41:46 +0800

    test: ✅ change config

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/**/*.js'],
+  collectCoverageFrom: ['lib/**/*.js'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
