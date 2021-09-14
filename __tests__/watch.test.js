0b02b29eecfeeebd555f47fa3df967b9aa329d32
Yoki Yu <yokiyuqian@gmail.com>
2021-09-14 11:22:42 +0800

    test: ✅ update watch.test.js

--- __tests__/watch.test.js
+++ __tests__/watch.test.js
@@ -1,13 +1,39 @@
 // const watch = require('../lib/watch');
-const watch = jest.createMockFromModule('../lib/watch')
+const watch = require('../lib/watch')
+const chokidar = require('chokidar')
+const childProcess = require('child_process')
+
+jest.spyOn(chokidar, 'watch')
+jest.mock('child_process')
 
 it('should be called', () => {
   watch()
   expect(watch).toHaveBeenCalled()
 })
 
-it('should be called with params', () => {
-  const params = ['**/*.*(test|spec).(js|ts|mjs)', 'echo changed']
-  watch(...params)
-  expect(watch).toHaveBeenCalledWith(...params)
+it.only('should be called with params', () => {
+  const paths = ['**/*.*(test|spec).(js|ts|mjs)']
+  const command = ['echo', '123']
+
+  const onCallback = jest.fn()
+  const emitCallback = jest.fn()
+
+  chokidar.watch.mockReturnValue({
+    on: onCallback,
+    emit: emitCallback,
+  })
+
+  watch(paths, command)
+  expect(chokidar.watch).toHaveBeenCalledWith(paths.concat('!node_modules'))
+
+  expect(onCallback.mock.calls[0][0]).toBe('all')
+  onCallback.mock.calls[0][1]()
+
+  expect(childProcess.spawn).toHaveBeenCalledWith(
+    command[0],
+    command.slice(1),
+    {
+      stdio: 'inherit',
+    }
+  )
 })
